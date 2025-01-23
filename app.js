07dc546780b8f4e88cb2b358154723f224003755
AKgit27 <akcoder7786@gmail.com>
2025-01-23 06:27:24 +0530

    addinitial app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Hello World!\n");
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello, World!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
+module.exports = app;
